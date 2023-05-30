0965e2298cdd0aadb6a6c08c0855aadbbd802a86
psychotimmy <timholyoake@btopenworld.com>
2023-05-30 18:28:28 +0100

    Update BigTest.pas
    Added tests for add, sub, multiply, divide, modulo and isqrt

--- BigTest.pas
+++ BigTest.pas
@@ -177,11 +177,165 @@ begin
   writeln('')
 end;
 
+procedure addition;
+var i: Integer;
+  res: BigInt;
+begin
+  res := '0';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := add(res,'1');
+  writeln('End value is ',res,' : ',i,' greater than start value to pass test');
+  writeln('');
+  res := '1';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := add(res,'1');
+  writeln('End value is ',res,' : ',i,' greater than start value to pass test');
+  writeln('');
+  res := '-1001';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := add(res,'1');
+  writeln('End value is ',res,' : ',i,' greater than start value to pass test');
+  writeln('');
+  writeln('The following results are undefined - adding non-integers ...');
+  writeln(add('A','k'));
+  writeln(add('--0','-0'));
+  writeln(add('',''))
+end;
+
+procedure subtraction;
+var i: Integer;
+  res: BigInt;
+begin
+  res := '0';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := sub(res,'1');
+  writeln('End value is ',res,' : ',i,' less than start value to pass test');
+  writeln('');
+  res := '-1';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := sub(res,'1');
+  writeln('End value is ',res,' : ',i,' less than start value to pass test');
+  writeln('');
+  res := '1001';
+  writeln('Start value is ',res);
+  for i := 1 to 1001 do
+    res := sub(res,'1');
+  writeln('End value is ',res,' : ',i,' less than start value to pass test');
+  writeln('');
+  writeln('The following results are undefined - subtracting non-integers ...');
+  writeln(sub('A','k'));
+  writeln(sub('--0','-0'));
+  writeln(sub('',''))
+end;
+
+procedure multiplication;
+begin
+  writeln('Next 4 results should all be 0 to pass');
+  writeln('');
+  write(multiply('0','1000'),', ');
+  write(multiply('1000','0'),', ');
+  write(multiply('0','-1000'),', ');
+  writeln(multiply('-1000','0'));
+  writeln('');
+  writeln('Next 4 results should all be -999 to pass');
+  writeln('');
+  write(multiply('1','-999'),', ');
+  write(multiply('-1','999'),', ');
+  write(multiply('999','-1'),', ');
+  writeln(multiply('-999','1'));
+  writeln('');
+  writeln('The following results are undefined - multiplying non-integers ...');
+  writeln(multiply('A','k'));
+  writeln(multiply('--0','-0'));
+  writeln(multiply('',''))
+end;
+
+procedure division;
+begin
+  writeln('Next 2 results should be 0, following 2 results NaN to pass');
+  writeln('');
+  write(divide('0','1000'),', ');
+  write(divide('0','-1000'),', ');
+  write(divide('-1000','0'),', ');
+  writeln(divide('1000','0'));
+  writeln('');
+  writeln('Next 2 results should be 20, follwing 2 results -20 to pass');
+  writeln('');
+  write(divide('100','5'),', ');
+  write(divide('-100','-5'),', ');
+  write(divide('100','-5'),', ');
+  writeln(divide('-100','5'));
+  writeln('');
+  writeln('The following results are undefined - dividing non-integers ...');
+  writeln(divide('A','k'));
+  writeln(divide('--0','-0'));
+  writeln(divide('',''))
+end;
+
+procedure modd;
+begin
+  writeln('Next 2 results should be 1, following 2 results -1 to pass');
+  writeln('');
+  write(modulo('51','50'),', ');
+  write(modulo('51','-50'),', ');
+  write(modulo('-51','50'),', ');
+  writeln(modulo('-51','-50'));
+  writeln('');
+  writeln('The following results are undefined - modulo non-integers ...');
+  writeln(modulo('A','k'));
+  writeln(modulo('--0','-0'));
+  writeln(modulo('',''))
+end;
+
+procedure squareroot;
+begin
+  writeln('Next result should be 1000, following result NaN to pass');
+  writeln('');
+  write(isqrt('1000000'),', ');
+  writeln(isqrt('-1000000'));
+  writeln('');
+  writeln('The following results are undefined - isqrt non-integers ...');
+  writeln(isqrt('A'));
+  writeln(isqrt('--0'));
+  writeln(isqrt(''))
+end;
+
 begin
    writeln('');
    writeln('Maximum size of BigInt is set to ',MAXLEN);
    writeln('');
    writeln('Starting compare ...');
    writeln('');
-   compare
+   compare;
+   writeln('');
+   writeln('Starting addition ...');
+   writeln('');
+   addition;
+   writeln('');
+   writeln('Starting subtraction ...');
+   writeln('');
+   subtraction;
+   writeln('');
+   writeln('Starting multiplication ...');
+   writeln('');
+   multiplication;
+   writeln('');
+   writeln('Starting division ...');
+   writeln('');
+   division;
+   writeln('');
+   writeln('Starting modulo ...');
+   writeln('');
+   modd;
+   writeln('');
+   writeln('Starting square root ...');
+   writeln('');
+   squareroot;
+   writeln('*** Tests finished ***');
+   writeln('')
 end.
